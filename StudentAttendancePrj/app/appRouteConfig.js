6ac4c572af332a66bd2241040914e58fddacf456
miyaji <qixinhui119@gmail.com>
2017-08-25 11:08:11 +0800

    [$injector:modulerr]问题
    jquery.min.js:2 Uncaught Error: [$injector:modulerr]未解决
    主界面完成
    各个功能部分尚未开始

---
+++ StudentAttendancePrj/app/appRouteConfig.js
@@ -0,0 +1,7 @@
+"use strict";
+
+angular.module("attendanceApp")
+    .config(["$routeProvider",function($routeProvider){
+
+        // console.log("config...");
+    }]);
---
+++ StudentAttendancePrj/ext-modules/framework/frameworkModule.js
@@ -0,0 +1,3 @@
+"use strict";
+
+angular.module('framework',['list','dashboard']);
